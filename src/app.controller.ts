5e439cd763abc10612a8ae015a8a00ec7fd9247a
刘畅 <10646242+liu-chang403@user.noreply.gitee.com>
2024-07-24 23:24:54 +0800

    模块内声明 providers对象，并注入使用

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  // 使用Inject装饰器 注入 在module文件声明的 provides 对象，这样就可以使用注入后的对象了
+  @Inject('person') private readonly person: { name: string; age: number };
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): { msg: string; name: string; age: number } {
+    const data = this.appService.getHello();
+    return {
+      msg: data,
+      age: this.person.age,
+      name: this.person.name,
+    };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,32 @@ import { PersonModule } from './person/person.module';
 @Module({
   imports: [PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'person',
+      useValue: {
+        name: 'LIUCHANG',
+        age: 18,
+      },
+    },
+  ],
 })
+/**
+ * providers 中声明对象时 useValue useFactory useClass useExisting 的区别
+ *
+ * useValue: 用于提供一个静态的值作为服务的实现; 可以是 字符串 对象 数字等任意类型;
+ * 在运行时，这个值会被直接返回，不会有额外的操作
+ *
+ * useFactory: 用于提供一个工厂函数，该函数在需要时会动态创建作为服务的实现
+ * 工厂函数可以接收其他服务作为依赖项；并根据需求动态创建实例
+ *
+ * useClass: 用于提供一个类作为服务的实现; 运行时,这个类会被实例化作为服务
+ * 这个类可以有自己的依赖项,通过构造函数注入
+ *
+ * useExisting: 用于提供一个现有的服务实例作为新的服务
+ * 这种情况下,新的服务只是现有服务的别名,他们共享一个实例
+ *
+ * 灵活运用这些 provider 类型，就可以利于 Nest 的 IOC 容器中注入的对象
+ */
 export class AppModule {}
